d3ac67c94a99ec8302bfa39d232fe2d03d8e4184
sehyun-kelly <sehyun.kelly.park@gmail.com>
2022-01-29 23:54:29 -0800

    Added Get Customer REST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React, { Component } from 'react';
 import Customer from './components/Customer'
 import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
@@ -7,58 +8,52 @@ import TableBody from '@mui/material/TableBody';
 import TableRow from '@mui/material/TableRow';
 import TableCell from '@mui/material/TableCell';
 
-const customers = [{
-    'id': '1',
-    'src': 'https://dummyimage.com/64x64/0da88c/fff.png&text=profile',
-    'name': 'Kelly Park',
-    'birthday': '920418',
-    'gender': 'Female',
-    'job': 'Student'
-  },
-  {
-    'id': '2',
-    'src': 'https://dummyimage.com/64x64/0da88c/fff.png&text=profile',
-    'name': 'Jun Park',
-    'birthday': '160322',
-    'gender': 'Male',
-    'job': 'Student'
-  },
-  {
-    'id': '3',
-    'src': 'https://dummyimage.com/64x64/0da88c/fff.png&text=profile',
-    'name': 'Aiden Park',
-    'birthday': '210710',
-    'gender': 'Male',
-    'job': 'Baby'
-  },
-]
 
-function App(props) {
-  return ( 
-    <Paper>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>ID</TableCell>
-            <TableCell>Image</TableCell>
-            <TableCell>Name</TableCell>
-            <TableCell>Date of Birth</TableCell>
-            <TableCell>Gender</TableCell>
-            <TableCell>Occupation</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-        {
-        customers.map(c => {
-          return (
-            <Customer id={c.id} src={c.src} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/ >
-          )
-        })
-      }
-        </TableBody>
-      </Table>
-    </Paper>
-  );
-}
+class App extends Component {
+  state = {
+    customers: ""
+  }
+
+  componentDidMount(){
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));
+  }
+ 
+  callApi = async() => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
+  render(){
+    return ( 
+      <Paper>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>ID</TableCell>
+              <TableCell>Image</TableCell>
+              <TableCell>Name</TableCell>
+              <TableCell>Date of Birth</TableCell>
+              <TableCell>Gender</TableCell>
+              <TableCell>Occupation</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+          {
+          this.state.customers ? 
+          this.state.customers.map(c => {
+            return (
+              <Customer id={c.id} src={c.src} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/ >
+            )
+          }) : ""}
+        
+          </TableBody>
+        </Table>
+      </Paper>
+    );
+  }
+    }
 
 export default App;
--- server.js
+++ server.js
@@ -6,8 +6,33 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
-app.get('/api/hello', (req, res) => {
-    res.send({message: 'Hello Express!'});
+app.get('/api/customers', (req, res) => {
+    res.send([
+        {
+            'id': '1',
+            'src': 'https://dummyimage.com/64x64/0da88c/fff.png&text=profile',
+            'name': 'Kelly Park',
+            'birthday': '920418',
+            'gender': 'Female',
+            'job': 'Student'
+          },
+          {
+            'id': '2',
+            'src': 'https://dummyimage.com/64x64/0da88c/fff.png&text=profile',
+            'name': 'Jun Park',
+            'birthday': '160322',
+            'gender': 'Male',
+            'job': 'Student'
+          },
+          {
+            'id': '3',
+            'src': 'https://dummyimage.com/64x64/0da88c/fff.png&text=profile',
+            'name': 'Aiden Park',
+            'birthday': '210710',
+            'gender': 'Male',
+            'job': 'Baby'
+          }
+    ]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
