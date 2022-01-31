7431c6fde2b23120a25d4e069934a6a5b6fa1d9d
sehyun-kelly <sehyun.kelly.park@gmail.com>
2022-01-30 22:02:01 -0800

    Updated design

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,13 @@ import TableHead from '@mui/material/TableHead';
 import TableBody from '@mui/material/TableBody';
 import TableRow from '@mui/material/TableRow';
 import TableCell from '@mui/material/TableCell';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
 
 class App extends Component {
@@ -43,19 +50,39 @@ class App extends Component {
   }
 
   render(){
+    const cellList = ["ID", "Image", "Name", "Date of Birth", "Gender", "Occupation", "Settings"];
     return ( 
       <div>
-      <Paper>
+        <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Customer Management System
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+    <div id="addbutton">
+    <CusomterAdd stateRefresh={this.stateRefresh}/>
+    </div>
+    <div id="paper">
+    <Paper>
         <Table>
           <TableHead>
             <TableRow>
-              <TableCell>ID</TableCell>
-              <TableCell>Image</TableCell>
-              <TableCell>Name</TableCell>
-              <TableCell>Date of Birth</TableCell>
-              <TableCell>Gender</TableCell>
-              <TableCell>Occupation</TableCell>
-              <TableCell>Settings</TableCell>
+              {cellList.map(c => {
+                return <TableCell >{c}</TableCell>
+              })}
             </TableRow>
           </TableHead>
           <TableBody>
@@ -70,7 +97,9 @@ class App extends Component {
           </TableBody>
         </Table>
       </Paper>
-      <CusomterAdd stateRefresh={this.stateRefresh}/>
+    </div>
+
+
       </div>
     );
   }
